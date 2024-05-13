748c551252a45a6e3a2e9d689196d6f2a84c441f
Emil <emil88337@gmail.com>
2024-05-13 12:48:50 +0300

    add config

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+
+from appium.options.android import UiAutomator2Options
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,3 +8,24 @@ user_name = os.getenv('LOGIN')
 access_key = os.getenv('PASSWORD')
 url = os.getenv('URL')
 time_out = float(os.getenv('TIMEOUT'))
+
+device_name = os.getenv('DEVICE_NAME')
+platform_version = os.getenv('PLATFORM_VERSION')
+remote_app = os.getenv('REMOTE_APP')
+
+options = UiAutomator2Options().load_capabilities({
+
+    "platformVersion": platform_version,
+    "deviceName": device_name,
+
+    "app": remote_app,
+
+    'bstack:options': {
+        "projectName": "First Python project",
+        "buildName": "browserstack-build-1",
+        "sessionName": "BStack first_test",
+
+        "userName": user_name,
+        "accessKey": access_key
+    }
+})
--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -1,7 +1,6 @@
 import pytest
-from appium.options.android import UiAutomator2Options
 from selene import browser
-from config import user_name, access_key, url, time_out
+import config
 import utils.allure
 from appium import webdriver
 import allure
@@ -9,34 +8,13 @@ import allure
 
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management():
-    options = UiAutomator2Options().load_capabilities({
-        # Specify device and os_version for testing
-        # "platformName": "android",
-        "platformVersion": "9.0",
-        "deviceName": "Google Pixel 3",
+    browser.config.driver_remote_url = config.url
 
-        # Set URL of the application under test
-        "app": "bs://sample.app",
+    browser.config.driver_options = config.options
 
-        # Set other BrowserStack capabilities
-        'bstack:options': {
-            "projectName": "First Python project",
-            "buildName": "browserstack-build-1",
-            "sessionName": "BStack first_test",
+    browser.config.timeout = config.time_out
 
-            # Set your access credentials
-            "userName": user_name,
-            "accessKey": access_key
-        }
-    })
-
-    browser.config.driver_remote_url = url
-
-    browser.config.driver_options = options
-
-    browser.config.timeout = time_out
-
-    browser.config.driver = webdriver.Remote(url, options=options)
+    browser.config.driver = webdriver.Remote(config.url, options=config.options)
 
     yield
 
